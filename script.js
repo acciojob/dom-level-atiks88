10c9dce5340c92397bdb3d2730b93064fc5b130e
atiks88 <141978852+atiks88@users.noreply.github.com>
2023-11-01 12:05:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+window.onload = function () {
+    let element = document.getElementById('level');
+    let level = 1;
+    while (element.parentElement) {
+        level++;
+        element = element.parentElement;
+    }
+    alert(`The level of the element is: ${level}`);
+};
